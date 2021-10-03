93db5dc9be00cfec6f3cee92a2cf26e91aae806a
wika12l <91103104+wika12l@users.noreply.github.com>
2021-10-03 17:34:36 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,255 @@
+
+var lee=document.getElementById("lang");
+var Kh=document.getElementById("khmer");
+function op1(){
+    Kh.style.display='block';
+    lee.style.display='none';
+
+}
+var lee=document.getElementById("lang");
+var En=document.getElementById("Eng");
+function op2(){
+    Kh.style.display='none';
+    En.style.display='block';
+    lee.style.display='none';
+
+}
+var mena=document.getElementById("ip1");
+var OK=document.getElementById("ok");
+function men1(){
+    OK.style.display='block';
+    mena.style.display='block';
+    mend.style.display='none';
+}
+var menb=document.getElementById("ip2");
+function men2(){
+    OK.style.display='block';
+    mena.style.display='none';
+    mend.style.display='none';
+}
+var menc=document.getElementById("ip3");
+function men3(){
+    OK.style.display='block';
+    mena.style.display='none';
+    mend.style.display='none';
+}
+var mend=document.getElementById("ip4");
+function men4(){
+    OK.style.display='block';
+    mend.style.display='block';
+    mena.style.display='none';
+}
+var opp1=document.getElementById("content1");
+var opp2=document.getElementById("content2");
+var opp3=document.getElementById("content3");
+var opp4=document.getElementById("content4");
+var opp5=document.getElementById("content5");
+var opp6=document.getElementById("content6");
+var opp7=document.getElementById("content7");
+var opp8=document.getElementById("content8");
+var opp9=document.getElementById("content9");
+var opp10=document.getElementById("content10");
+var opp11=document.getElementById("content11");
+var opp12=document.getElementById("content12");
+var customalert=document.getElementById("customAlert");
+function showcustom(){
+    var ao=document.getElementById("name").value;
+    var a=document.getElementById("old").value;
+
+    if(a=="14")
+    {
+        opp1.style.display='block';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="15")
+    {
+        opp1.style.display='none';
+        opp2.style.display='block';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="16")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='block';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="17")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='block';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="18")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='block';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="19")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='block';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="20")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='block';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="21")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='block';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="22")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='block';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="23")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='block';
+        opp12.style.display='none';
+        customalert.style.display='block';
+    }
+    else if(a=="24")
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp4.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='block';
+        customalert.style.display='block';
+    }
+    else
+    {
+        opp1.style.display='none';
+        opp2.style.display='none';
+        opp3.style.display='none';
+        opp5.style.display='none';
+        opp6.style.display='none';
+        opp7.style.display='none';
+        opp8.style.display='none';
+        opp9.style.display='none';
+        opp10.style.display='none';
+        opp11.style.display='none';
+        opp12.style.display='none';
+        opp4.style.display='block';
+        customalert.style.display='block';
+    }
+}
+function hide(){
+    customalert.style.display='none';
+}
+
