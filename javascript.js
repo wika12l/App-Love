cd14804e4703e0dd5c1b9d3e64bac4313256cdf5
wika12l <91103104+wika12l@users.noreply.github.com>
2021-10-29 07:39:50 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,35 @@
-var vso=document.getElementById("amenu");
-var logo1=document.getElementById("menu-logo-phone1");
-var hlo=document.getElementById("home-logo-phone1");
-function vdo(){
-    vso.style.display='none';
-    $(document).ready(function(){
-        $('#menu-logo-phone1').click(function(){
-           $('.items-menu-phone').slideToggle();
-           $('.head-menu-phone').slideToggle();
-        });
-    });
-    hlo.style.display='none';
-    logo1.style.display='block';
+var vei=document.getElementById("po");
+var ne=document.getElementById("new");
+var a1=document.getElementById("admin");
+var a2=document.getElementById("about");
+var h=document.getElementById("help");
+var pil=document.getElementById("menu-logo-phone0");
+var pl=document.getElementById("menu-logo-phone1");
+function okk(){
+    vei.style.display='block';
+    pl.style.display='none';
+    ne.style.display='none';
 }
-$(document).ready(function(){
-    $('.mu2').click(function(){
-       $('.BB').slideToggle();
-    });
-});
-
+function clos(){
+    ne.style.display='none';
+    pl.style.display='block';
+    vei.style.display='none';
+}
+function admin(){
+    ne.style.display='block';
+    a1.style.display='block';
+    a2.style.display='none';
+    h.style.display='none';
+}
+function about(){
+    a2.style.display='block';
+    ne.style.display='block';
+    a1.style.display='none';
+    h.style.display='none';
+}
+function help(){
+    a1.style.display='none';
+    a2.style.display='none';
+    h.style.display='block';
+    ne.style.display='block';
+}
